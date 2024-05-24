93f3c29a6638eaf7e733a4d3e5b574a4b154f676
cmc <cmc@163.com>
2024-05-24 15:26:50 +0800

    新增打ZIP包插件

---
+++ project/bin/run.bat
@@ -0,0 +1,1 @@
+java -cp ./lib/mytest-1.0-SNAPSHOT.jar com.doit.projects.gui.ApplicationServer
---
+++ project/bin/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nohup java -cp ./lib/mytest-1.0-SNAPSHOT.jar com.doit.projects.gui.ApplicationServer &
